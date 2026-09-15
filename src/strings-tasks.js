f9db0605f20193d79b231c61a6bf14252532d1d8
Svyatoslav Pankov <spankov@ikod.kz>
2026-09-15 10:09:50 +0500

    feat: implement isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -334,8 +334,10 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const chars = [...str.toLowerCase()].filter((char) => /[a-z]/.test(char));
+
+  return chars.join('') === chars.reverse().join('');
 }
 
 /**
