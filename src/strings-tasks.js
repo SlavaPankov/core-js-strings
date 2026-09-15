d3647978d2afa17aef8962badb76257429f2c42a
Svyatoslav Pankov <spankov@ikod.kz>
2026-09-15 10:27:54 +0500

    feat: implement encodeToRot13

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -483,8 +483,12 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  return str.replace(/[a-zA-Z]/g, (char) => {
+    const base = char <= 'Z' ? 65 : 97;
+
+    return String.fromCharCode(((char.charCodeAt(0) - base + 13) % 26) + base);
+  });
 }
 
 /**
